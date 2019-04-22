3c13efe1ff13d8acdcd7106fa6628046dd56f076
Maksim Gordeev <mxmgrd@gmail.com>
2019-04-22 18:29:29 +0400

    http serve

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
+
+	echoHandler := func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprintf(w, "Echo response")
+	}
+
 	fmt.Println("Hello Go!")
 	fmt.Println("Arguments:", os.Args)
 	// fmt.Println("Environments:", os.Environ())
+	http.HandleFunc("/echo", echoHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
